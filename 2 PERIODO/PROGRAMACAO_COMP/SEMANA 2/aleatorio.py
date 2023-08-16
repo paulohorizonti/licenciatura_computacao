817f1c68a24049d40f943d5ad622c25c0536a56d
paulohorizonti <pnagi2012@gmail.com>
2023-08-16 08:17:38 -0300

    COMMIT SEM OS TRABALHOS XP

--- 2 PERIODO/PROGRAMACAO_COMP/SEMANA 2/aleatorio.py
+++ 2 PERIODO/PROGRAMACAO_COMP/SEMANA 2/aleatorio.py
@@ -1,17 +1,10 @@
-numero = 5
+numero1 = int(input("Digite o primeiro número inteiro: "))
+numero2 = int(input("Digite o segundo número inteiro: "))
+numero3 = float(input("Digite o número real: "))
+resultado1 = (numero1 * 2) * (numero2 / 2)
+resultado2 = (numero1 * 3) + numero3
+resultado3 = numero3 ** 3
 
-nome = Maria
-
-valor = 9.99
-
-ativo = True
-
-
-print("Número:", numero)
-
-print("Nome:", nome)
-
-print("Valor:", valor)
-
-print("Ativo:", ativo)
-
+print("O produto do dobro do primeiro com metade do segundo é:", resultado1)
+print("A soma do triplo do primeiro com o terceiro é:", resultado2)
+print("O terceiro elevado ao cubo é:", resultado3)
